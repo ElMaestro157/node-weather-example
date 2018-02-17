8d5949f7fbe2c6be4a90fb99af167243b794b54b
Dmitry Popov <Dimapopov.ru@yandex.ru>
2018-02-17 22:15:56 +0300

    formatting problems resolved

--- src/index.js
+++ src/index.js
@@ -30,61 +30,55 @@ class Search extends React.Component {
 
   render() {
     const {cities, text} = this.state;
-    return (<div >
-      < input="input" type="text" onChange={this.onChange} value={text}/>
-      < div="div">
-        {
-          cities.length
-            ? (< span onClick = {
-              this.chooseHandler
-            } > {
-              cities[0]
-            } < /span>
-                ) : null
-            }
-        < /div>
-          < /div>
-            ); } } const Weather = props => props.weather ? ( < div > < div > {`country: ${props.weather.country}`}
-            < /div>
-              < div="div">
-                {`temperature: ${props.weather.temperature}`}
-                < /div>
-                  < div="div">
-                    {`cloud: ${props.weather.cloud}`}
-                    < /div>
-                      < div="div">
-                        {`condition: ${props.weather.condition}`}
-                        < /div>
-                          < img="img" src={props.weather.conditionIcon}/>
-                          < /div>
-                            ) : null; class App extends React.Component {
-                              constructor(props) {
-                                super(props);
-
-                                this.state = {
-                                  weather: null
-                                }
-                              }
-                              chooseCityCallback = (city) => {
-                                fetch(`/weather?city=${city}`).then(res => res.json()).then(({weather}) => {
-                                  this.setState({weather});
-                                });
-                              }
-
-                              render() {
-                                return (< div > <Search chooseCityCallback = {
-                                  this.chooseCityCallback
-                                } /> <Weather weather = {
-                                  this.state.weather
-                                } /> < /div>
-        )
-    }
+    return (
+      <div>
+        <input type="text" onChange={this.onChange} value={text}/>
+          <div>
+          {
+            cities.length 
+            ? (<span onClick={this.chooseHandler}>{cities[0]}</span>) 
+            : null
+          }
+          </div>
+      </div>
+    );
+  }
 }
 
-                            class Man extends React.Component {
-                              render() {
-                                return (< div > < /div>
+const Weather = props => props.weather ? (
+  <div>
+    <div>{`country: ${props.weather.country}`}</div>
+    <div>{`temperature: ${props.weather.temperature}`}</div>
+    <div>{`cloud: ${props.weather.cloud}`}</div>
+    <div>{`condition: ${props.weather.condition}`}</div>
+    <img src={props.weather.conditionIcon}/>
+  </div>
+  ) : null;
+
+  class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        weather: null
+      }
+    }
+
+    chooseCityCallback = (city) => {
+      fetch(`/weather?city=${city}`)
+      .then(res => res.json())
+      .then(({weather}) => {
+        this.setState({weather});
+      });
+    }
+
+    render() {
+      return (
+        <div>
+          <Search chooseCityCallback={this.chooseCityCallback}/>
+          <Weather weather={this.state.weather}/>
+        </div>
         );
     }
 }
-                            ReactDOM.render( < App / > , document.querySelector('#app'));
+
+ReactDOM.render(<App /> , document.querySelector('#app'));
